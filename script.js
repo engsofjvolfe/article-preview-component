3ade6e57b32ccfe84a57b5dc90f68ab2283a280f
Jeanco Volfe <engsofjvolfe@gmail.com>
2023-02-18 01:59:14 -0300

    JS optimized for mobile event

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
+let isTouchDevice = 'ontouchstart' in document.documentElement;
 let shareShow = document.querySelector(".main__contact--share");
-let shareHide = document.querySelector(".links--share")
 let container = document.querySelector(".main__contact__socials");
 
+
+if (isTouchDevice) {
+  shareShow.addEventListener('click', function() {
+    container.classList.toggle('expand');
+    console.log("isTouch")
+  });
+} else {
 shareShow.addEventListener("mouseenter", function (e) {
   e.stopPropagation()
   container.classList.add("expand");
 });
 
-shareHide.addEventListener("mouseleave", function (e) {
+
+shareShow.addEventListener("mouseleave", function (e) {
   e.stopPropagation();
   container.classList.remove("expand");
 });
+}
+
+
